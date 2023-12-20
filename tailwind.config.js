fb9ccc8c610f93f3a595e13e6b72e5a695760b1b
Saurabh <apple@Apples-Laptop.local>
2023-12-20 13:31:26 +0530

    update: removed the default margin for tailwind prose

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,31 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            p: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+            h1: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+            h2: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+            h3: {
+              marginTop: "0",
+              marginBottom: "0",
+            },
+            // Add other elements as needed
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
